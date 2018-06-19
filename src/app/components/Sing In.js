09d348416304baea8f88d818ffc7976f8d8a2bd2
Julia <qtuvyxazh2bl@mail.ru>
2018-06-20 00:33:03 +0300

    correct

--- src/app/components/Sing In.js
+++ src/app/components/Sing In.js
@@ -2,29 +2,11 @@ import React from "react";
 import { Forms } from "./Forms";
 
 export class SingIn extends React.Component{
-    constructor(props){
-        super();
-        this.state={
-            homeLink: "Hellooooo"
-        };
-    }
-
-
-    onSendForm0(){
-        if(this.homeLink.length<6){
-            alert ("Недопустимая длина строки");
-            document.form0.reset();
-            event.preventDefault();
-        } else
-            alert ("Отправка разрешена");
-    }
     render(){
         return(
             <div className="navbar navbar-inverse">
                 <div className="navbar-header">
-                    <Forms
-                        homeLink={this.state.homeLink}
-                         />
+                    <Forms />
                 </div>
             </div>
         );
