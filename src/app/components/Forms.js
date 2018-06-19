8faf8493289b4c40f7735eafa85e873c614005d4
Julia <qtuvyxazh2bl@mail.ru>
2018-06-20 00:18:10 +0300

    correct

--- src/app/components/Forms.js
+++ src/app/components/Forms.js
@@ -4,32 +4,15 @@ import { render } from "react-dom";
 
 
 export class Forms extends React.Component {
-    constructor(){
-        super();
-        this.state={
-
-        };
-    }
-
-
-    onSendForm0(){
-        let keyBox = document.form0;
-        let val = keyBox.value;
-        if(val.length<6){
-            alert ("Недопустимая длина строки");
-            document.form0.reset();
-            event.preventDefault();
-        } else
-            alert ("Отправка разрешена");
-    }
 
     render(){
         return(
             <div>
+                <p>{this.props.homeLink}</p>
                 <h2 className="text-center" id="log">Вход в личный кабинет</h2>
                         <input type="text" name="input0" placeholder="Enter username" required />
                         <input type="text" name="input0.1" placeholder="Enter password" required />
-                        <button  onClick={()=> this.onSendForm0()} className="btn btn-primary" name="btn0" type="submit"><i className="fa fa-paper-plane"></i> Sign In</button>
+                        <button  onClick={this.props.greet} className="btn btn-primary" name="btn0" type="submit"><i className="fa fa-paper-plane"></i> Sign In</button>
                         <p><em><a className="links" href="#" target="_blank">Forgot your username or password?</a></em></p>
             </div>
         );
--- src/app/components/Sing In.js
+++ src/app/components/Sing In.js
@@ -2,12 +2,29 @@ import React from "react";
 import { Forms } from "./Forms";
 
 export class SingIn extends React.Component{
+    constructor(props){
+        super();
+        this.state={
+            homeLink: "Hellooooo"
+        };
+    }
 
+
+    onSendForm0(){
+        if(this.homeLink.length<6){
+            alert ("Недопустимая длина строки");
+            document.form0.reset();
+            event.preventDefault();
+        } else
+            alert ("Отправка разрешена");
+    }
     render(){
         return(
             <div className="navbar navbar-inverse">
                 <div className="navbar-header">
-                    <Forms />
+                    <Forms
+                        homeLink={this.state.homeLink}
+                         />
                 </div>
             </div>
         );
