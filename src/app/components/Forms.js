2ec74aee3301778401afde123e7c7e3b4640b74e
Julia <julia.avsyuk@mail.ru>
2018-06-20 16:51:33 +0300

    добавили обработку на первый инпут

--- src/app/components/Forms.js
+++ src/app/components/Forms.js
@@ -4,18 +4,36 @@ import { render } from "react-dom";
 
 
 export class Forms extends React.Component {
-    onChange(){
-        alert("Hello1");
+    constructor(props){
+        super(props);
+        this.state = {
+            name: ""
+        };
+        this.onChange = this.onChange.bind(this);
+        this.preSubmit = this.preSubmit.bind(this);
     }
+
+    onChange(e){
+        let val = e.target.value;
+        this.setState({
+            name: val
+        });
+    }
+
+    preSubmit(e){
+        e.preventDefault();
+        alert("Login: " + this.state.name);
+    }
+
     render(){
         return(
-            <div>
+            <form onSubmit={this.preSubmit}>
                 <h2 className="text-center" id="log">Вход в личный кабинет</h2>
-                        <input type="text" name="input0" placeholder="Enter username" required />
-                        <input type="text" name="input0.1" placeholder="Enter password" required />
-                        <button  onClick={this.onChange} className="btn btn-primary" name="btn0" type="submit"><i className="fa fa-paper-plane"></i> Sign In</button>
-                        <p><em><a className="links" href="#" target="_blank">Forgot your username or password?</a></em></p>
-            </div>
+                <input type="text" value={this.state.name} onChange={this.onChange} placeholder="Enter username" required />
+                <input type="text" placeholder="Enter password" required />
+                <button className="btn btn-primary" type="submit"><i className="fa fa-paper-plane"></i> Sign In</button>
+                <p><em><a className="links" href="#" target="_blank">Forgot your username or password?</a></em></p>
+            </form>
         );
     }
 }
