d8cb8077800a39f18327d5fe8f18fda3dc97d362
Julia <julia.avsyuk@mail.ru>
2018-06-20 16:57:52 +0300

    correct variable

--- src/app/components/Forms.js
+++ src/app/components/Forms.js
@@ -7,7 +7,7 @@ export class Forms extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            name: ""
+            log: ""
         };
         this.onChange = this.onChange.bind(this);
         this.preSubmit = this.preSubmit.bind(this);
@@ -16,20 +16,20 @@ export class Forms extends React.Component {
     onChange(e){
         let val = e.target.value;
         this.setState({
-            name: val
+            log: val
         });
     }
 
     preSubmit(e){
         e.preventDefault();
-        alert("Login: " + this.state.name);
+        alert("Login: " + this.state.log);
     }
 
     render(){
         return(
             <form onSubmit={this.preSubmit}>
                 <h2 className="text-center" id="log">Вход в личный кабинет</h2>
-                <input type="text" value={this.state.name} onChange={this.onChange} placeholder="Enter username" required />
+                <input type="text" value={this.state.log} onChange={this.onChange} placeholder="Enter username" required />
                 <input type="text" placeholder="Enter password" required />
                 <button className="btn btn-primary" type="submit"><i className="fa fa-paper-plane"></i> Sign In</button>
                 <p><em><a className="links" href="#" target="_blank">Forgot your username or password?</a></em></p>
