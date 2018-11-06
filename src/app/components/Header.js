c1fb8f091643b875f6bcac28e8fc30cd22caa1b1
Julia <julia.avsyuk@mail.ru>
2018-11-06 18:00:54 +0300

    no message

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export class Header extends React.Component{
     render(){
         return(
-            <div>
+            <div className="main_page">
                 <ul className="nav nav-bar-nav">
                     <li><NavLink to="/home">Home</NavLink></li>
                     <li><NavLink to="/contacts">Contacts</NavLink></li>
